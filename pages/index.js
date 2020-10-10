1f1604c264f51d147eef76bdaa67eb22e027d5f6
Ricardo Dantas <ricardodantas@gmail.com>
2020-10-10 15:55:56 +0200

    update css and html + remove unused files

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,73 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faTwitter,
+	faGithub,
+	faLinkedin,
+	faInstagram,
+	faMedium,
+	faGoodreads,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faRssSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Ricardo Dantas | Full Stack Developer</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+			<main className={styles.main}>
+				<img
+					className={styles.photo}
+					src={
+						"https://www.gravatar.com/avatar/742396bc8754671336d09ee4ac530f5a?s=280"
+					}
+				/>
+				<h1 className={styles.title}>Ricardo Dantas</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+				<p className={styles.description}>Full Stack Developer</p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+				<ul className={styles.socialMedia}>
+					<li>
+						<a href={`https://www.linkedin.com/in/rdantas`} target="_blank">
+							<FontAwesomeIcon icon={faLinkedin} />
+						</a>
+					</li>
+					<li>
+						<a href={`https://github.com/ricardodantas`} target="_blank">
+							<FontAwesomeIcon icon={faGithub} />
+						</a>
+					</li>
+					<li>
+						<a href={`https://twitter.com/ricardodantas`} target="_blank">
+							<FontAwesomeIcon icon={faTwitter} />
+						</a>
+					</li>
+					<li>
+						<a href={`https://medium.com/@ricardodantas`} target="_blank">
+							<FontAwesomeIcon icon={faMedium} />
+						</a>
+					</li>
+					<li>
+						<a
+							href={`https://www.goodreads.com/user/show/77647522-ricardo-dantas`}
+							target="_blank"
+						>
+							<FontAwesomeIcon icon={faGoodreads} />
+						</a>
+					</li>
+					<li>
+						<a href={`mailto:ricardodantas@gmail.com`}>
+							<FontAwesomeIcon icon={faEnvelope} />
+						</a>
+					</li>
+				</ul>
+			</main>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+			{/* <footer className={styles.footer}></footer> */}
+		</div>
+	);
 }
