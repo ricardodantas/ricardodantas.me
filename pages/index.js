d65221ef410423e4455d12c5957d7a7189dea43f
Ricardo Dantas <ricardodantas@gmail.com>
2021-05-02 20:24:26 +0200

    update to use tailwind CSS

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,67 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { getPerson } from '../libs/api';
 import { ReactSVG } from 'react-svg';
 
 export default function Home({ person }) {
-	const allSocialMedias = person.socialMediaCollection?.items || [];
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>{person.name} | { person.jobTitle}</title>
-				<link rel="icon" href={`${person.photo.url}?w=100&h=1000&q=90&fm=webp`} />
-				<script
-					async
-					src="https://www.googletagmanager.com/gtag/js?id=UA-124612577-1"
-				></script>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-
-							window.dataLayer = window.dataLayer || []; function gtag()
-							{dataLayer.push(arguments)}
-							gtag('js', new Date()); gtag('config', 'UA-124612577-1');
+  const allSocialMedias = person.socialMediaCollection?.items || [];
+  return (
+    <>
+      <Head>
+        <title>{person.name} | { person.jobTitle}</title>
+        <link rel="icon" href={`${person.photo.url}?w=100&h=1000&q=90&fm=webp`} />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-124612577-1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || []; function gtag()
+              {dataLayer.push(arguments)}
+              gtag('js', new Date()); gtag('config', 'UA-124612577-1');
               `,
-					}}
-				/>
-			</Head>
+          }}
+        />
+      </Head>
 
-			<main className={styles.main}>
+      <main className="bg-gray-100 dark:bg-gray-900 dark:text-gray-500 h-screen flex flex-col items-center justify-center overflow-hidden">
 
-				<img
-					className={styles.photo}
-					src={`${person.photo.url}?w=800&h=800&q=90&fm=webp`}
-				/>
+        <div className="flex flex-col items-center justify-center">
 
-				<h1 className={styles.title}>{person.name}</h1>
+          <div className="w-60 h-60 rounded-full border-4 dark:border-gray-700 border-white shadow bg-cover bg-no-repeat" style={{backgroundImage: `url(${person.photo.url}?w=800&h=750&q=90&fm=webp)`}}></div>
 
-				<p className={styles.description}>{ person.jobTitle }</p>
+          <div className="mt-7">
+            <h1 className="text-center mb-1 font-normal text-3xl dark:text-gray-400">{person.name}</h1>
+            <p className="text-lg text-center font-extralight	 dark:text-gray-400 mb-3">{ person.jobTitle }</p>
+          </div>
 
-				<ul className={styles.socialMedia}>
-					{allSocialMedias.map(socialMedia => (
-						<li key={socialMedia.name}>
-							<a href={socialMedia.url} target="_blank">
-								<ReactSVG src={socialMedia.icon.url} className={styles.socialMediaIcon} wrapper={'span'}/>
-							</a>
-						</li>
-					))}
-					<li>
-						<a href={`mailto:${person.email}`}>
-							<FontAwesomeIcon icon={faEnvelope} className={ styles.emailIcon} />
-						</a>
-					</li>
-				</ul>
-			</main>
 
-			{/* <footer className={styles.footer}></footer> */}
-		</div>
-	);
+          <div className="flex flex-row items-center justify-center p-3">
+            {allSocialMedias.map(socialMedia => (
+              <div className="w-11 px-2 hover:opacity-50 transition duration-500" key={socialMedia.name}>
+                <a href={socialMedia.url} target="_blank">
+                  <ReactSVG src={socialMedia.icon.url} className="fill-current"/>
+                </a>
+              </div>
+            ))}
+            <div className="w-14 px-3 hover:opacity-50 transition duration-500">
+              <a href={`mailto:${person.email}`}>
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 
 export async function getStaticProps({ preview = false }) {
-	const person = await getPerson(preview);
-	return {
-		props: { preview, person },
-	};
+  const person = await getPerson(preview);
+  return {
+    props: { preview, person },
+  };
 }
